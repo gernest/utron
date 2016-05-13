b78ebfd414769ed05767e198a8c094dedd2bf925
gernest <geofreyernest@live.com>
2016-05-13 06:17:44 +0300

    Fix gometalinter errors

--- fixtures/todo/controllers/todo.go
+++ fixtures/todo/controllers/todo.go
@@ -11,11 +11,13 @@ import (
 
 var decoder = schema.NewDecoder()
 
+//TODO is a controller for Todo list
 type TODO struct {
 	*utron.BaseController
 	Routes []string
 }
 
+//Home renders a todo list
 func (t *TODO) Home() {
 	todos := []*models.Todo{}
 	t.Ctx.DB.Order("created_at desc").Find(&todos)
@@ -23,10 +25,12 @@ func (t *TODO) Home() {
 	t.Ctx.Template = "index"
 	t.HTML(http.StatusOK)
 }
+
+//Create creates a todo  item
 func (t *TODO) Create() {
 	todo := &models.Todo{}
 	req := t.Ctx.Request()
-	req.ParseForm()
+	_ = req.ParseForm()
 	if err := decoder.Decode(todo, req.PostForm); err != nil {
 		t.Ctx.Data["Message"] = err.Error()
 		t.Ctx.Template = "error"
@@ -38,6 +42,7 @@ func (t *TODO) Create() {
 	t.Ctx.Redirect("/", http.StatusFound)
 }
 
+//Delete deletes a todo item
 func (t *TODO) Delete() {
 	todoID := t.Ctx.Params["id"]
 	ID, err := strconv.Atoi(todoID)
@@ -51,6 +56,7 @@ func (t *TODO) Delete() {
 	t.Ctx.Redirect("/", http.StatusFound)
 }
 
+//NewTODO returns a new  todo list controller
 func NewTODO() *TODO {
 	return &TODO{
 		Routes: []string{
