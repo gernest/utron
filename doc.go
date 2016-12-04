e06e0ed5ff970b3206c87526483101fb42c33f90
gernest <geofreyernest@live.com>
2016-12-04 14:33:37 +0300

    Update godoc

--- doc.go
+++ doc.go
@@ -1,16 +1,2 @@
-/*
-Package utron is a lightweight MVC framework for building fast, scalable and robust web applications
-
-example hello world in utron
-
-	type Hello struct {
-		*BaseController
-	}
-
-	func (h *Hello) World() {
-		h.Ctx.Write([]byte("hello world"))
-		h.String(http.StatusOK)
-	}
-
-*/
+//Package utron is a lightweight MVC framework for building fast, scalable and robust web applications
 package utron
