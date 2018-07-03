89201996457918b1806c677cb469f3dce59754b1
NlaakStudios <andrew@nlaak.com>
2018-07-02 20:42:37 -0500

    renamed last few ultrons in readme

--- doc.go
+++ doc.go
@@ -1,10 +1,10 @@
 //Package gowaf is a lightweight MVC framework for building fast, scalable and robust web applications
 //
-// Utron is built with focus of composability. This means, you can reap all the
-// benefit of using utron while maintaining harmony with existing libraries and
+// gowaf is built with focus of composability. This means, you can reap all the
+// benefit of using gowaf while maintaining harmony with existing libraries and
 // frameworks.
 //
-// Core feaures of utron
+// Core feaures of gowaf
 //
 // * Session management
 //
@@ -20,14 +20,14 @@
 //
 // * Static content serving
 //
-// Why utron
+// Why gowaf
 //
 // There are many frameworks out there, you might be wondering why do we need
-// yet another framework?. We probably don't.. Utron is just a summary of the
+// yet another framework?. We probably don't.. gowaf is just a summary of the
 // tools, and techniques I use to develop web  applications in Go.
 //
 // This includes the best libraries, and the best organization of the code base.
-// Utron has one of the very handy Controller( you will see more details in the
+// gowaf has one of the very handy Controller( you will see more details in the
 // controller section)
 //
 // These are the common libraries I use
@@ -39,7 +39,7 @@
 // * gorm: for object relationl mapping.
 //
 // In all of my web application I noticed that, keeping global state brings a
-// lot of complexities to the application, so uttron avoids this. utron
+// lot of complexities to the application, so uttron avoids this. gowaf
 // applicatio is a simple struct that you can play around with in whatever way
 // that you find suits you.
 package gowaf
