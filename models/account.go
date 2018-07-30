c87e6e539a855d8df3a4af9c6fff52deb40b2200
NlaakStudios <andrew@nlaak.com>
2018-07-30 09:58:12 -0500

    - added formatted output interface to Account Model

--- models/account.go
+++ models/account.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/asaskevich/govalidator"
@@ -14,22 +15,58 @@ type Account struct {
 	UpdatedAt      time.Time `schema:"updated"`
 	Username       string    `valid:"required,length(6|16)" schema:"username"`
 	Password       string    `gorm:"-" valid:"required,length(6|24)" schema:"password"`
-	Email          string    `valid:"required,email" schema:"email"`
+	EmailID        int       `schema:"email_id"`
+	Email          Email     `gorm:"foreignkey:EmailID"`
 	VerifyPass     string    `gorm:"-" schema:"verifypass"`
+	CompanyID      int       `schema:"company_id"`
+	Company        Company   `gorm:"foreignkey:CompanyID"`
+	PersonID       int       `schema:"person_id"`
+	Person         Person    `gorm:"foreignkey:PersonID"`
 	HashedPassword string
-	CompanyID      int     `schema:"company_id"`
-	Company        Company `gorm:"foreignkey:CompanyID"`
-	PersonID       int     `schema:"person_id"`
-	Person         Person  `gorm:"foreignkey:PersonID"`
+}
+
+// SingleLine returns a formatted single line text representing the Model
+// {Username}: {Email.Address} [{ID},{CID},{PID}]}
+func (m *Account) SingleLine() string {
+	return fmt.Sprintf("%s: %s [%d, %d, %d]",
+		m.Username,
+		m.Email.SingleLine(),
+		m.ID,
+		m.CompanyID,
+		m.PersonID,
+	)
+}
+
+// MultiLine returns a formatted multi-line text representing the Model
+// {Username}: {Person.SingleLine()}
+// {Email.Address}
+// {Company.SingleLine()}
+func (m *Account) MultiLine() string {
+	return fmt.Sprintf("%s: %s\n%s\n%s\n",
+		m.Username,
+		m.Person.PersonName.SingleLine(),
+		m.Email.Address,
+		m.Company.SingleLine(),
+	)
+}
+
+// HTMLView returns a HTML5 code representing a view of the Model
+func (m *Account) HTMLView() string {
+	return "<div id=\"AccountHTMLView\">{View Content}</div>"
+}
+
+// HTMLForm returns a HTML5 code representing a form of the Model
+func (m *Account) HTMLForm() string {
+	return "<div id=\"AccountHTMLForm\">{Form Content}</div>"
 }
 
 // Validate is used to verifiy password hash match
-func (u *Account) Validate() error {
-	_, err := govalidator.ValidateStruct(u)
+func (m *Account) Validate() error {
+	_, err := govalidator.ValidateStruct(m)
 	if err != nil {
 		return err
 	}
-	if u.Password != u.VerifyPass {
+	if m.Password != m.VerifyPass {
 		return errors.New("Model.Account: Password missmatch")
 	}
 	return err
